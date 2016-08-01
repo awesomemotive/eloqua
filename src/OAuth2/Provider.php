e1b6f4c163c337ecefc66b169752b0301af82940
Thomas Griffin <jthomas.griffin@gmail.com>
2016-08-01 10:08:23 -0400

    Implement required abstract methods.

--- src/OAuth2/Provider.php
+++ src/OAuth2/Provider.php
@@ -33,4 +33,29 @@ class Provider extends AbstractProvider {
 
 	}
 
+	/**
+	 * Get the URL that this provider uses to request user details.
+	 * Since this URL is typically an authorized route, most providers will require you to pass the access_token as
+	 * a parameter to the request. For example, the google url is:
+	 * 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='.$token
+	 *
+	 * @param AccessToken $token
+	 * @return string
+	 */
+	public function urlUserDetails( AccessToken $token ) {
+		// TODO: Implement urlUserDetails() method.
+	}
+
+	/**
+	 * Given an object response from the server, process the user details into a format expected by the user
+	 * of the client.
+	 *
+	 * @param object      $response
+	 * @param AccessToken $token
+	 * @return mixed
+	 */
+	public function userDetails( $response, AccessToken $token ) {
+		// TODO: Implement userDetails() method.
+	}
+
 }
